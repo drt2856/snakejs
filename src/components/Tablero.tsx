276621e1faa161b28b3bd296b83bc10e45245adb
drtorres <drt2856@gmail.com>
2024-08-16 14:45:58 -0400

    Commit para trabajar en casa  cambios serios con el prevState incompletos

--- src/components/Tablero.tsx
+++ src/components/Tablero.tsx
@@ -2,66 +2,71 @@ import React, { useState, useEffect } from 'react';
 import { configs, DIRECTIONS, OBJECTS } from '../App.tsx';
 import { Label } from './Label.tsx';
 
-export function Tablero({setStart}) {
+export function Tablero({ setStart }) {
 
-
-
-  const [loser, setLuser] = useState(false);
-  const [pointsB, setPointsB] = useState(0);
-  const [count, setCount] = useState(0);
-  const [direction, setDirection] = useState(DIRECTIONS.UP);
-  const [velosityB, setVelosityB] = useState(configs.velosityDefault);
+  const [inGame, setInGame] = useState(true);
   const [matriz, setMatriz] = useState([]);
   const [apple, setApple] = useState({ X: 0, Y: 0, whatIs: OBJECTS.FAST_POWER });
   const [power, setPower] = useState({ X: 0, Y: 0 });
-  const [snakeB, setSnakeB] = useState([{ X: 5, Y: 5 }, { X: 5, Y: 6 }, { X: 5, Y: 7 }]);
+  const [snakeB, setSnakeB] = useState({
+    sections: [
+      { X: 5, Y: 5 },
+      { X: 5, Y: 6 },
+      { X: 5, Y: 7 }
+    ],
+    velosity: configs.velosityDefault,
+    direction: DIRECTIONS.UP,
+    points: 0
+  }
+
+  );
 
 
 
   useEffect(() => {
-    function handleKeyDown(event) {
-      console.log(event.key);
+    //crear la matriz
+    createMatrix(configs.cant_fil, configs.cant_col)
+    //mover el maja por cada vez que la velocidad lo indique
+    const intervalId = setInterval(() => {
+      moveSnakeB()
+    }, snakeB.velosity)
 
+    function handleKeyDown(event) {
       if (event.key === "w") {
-        setDirection(DIRECTIONS.UP)
+        setSnakeB((prevState) => ({ ...prevState, direction: DIRECTIONS.UP }));
       }
       if (event.key === "a") {
-        setDirection(DIRECTIONS.LEFT)
+        setSnakeB((prevState) => ({ ...prevState, direction: DIRECTIONS.LEFT }));
       }
       if (event.key === "s") {
-        setDirection(DIRECTIONS.DOWN)
+        setSnakeB((prevState) => ({ ...prevState, direction: DIRECTIONS.DOWN }));
       }
       if (event.key === "d") {
-        setDirection(DIRECTIONS.RIGHT)
+        setSnakeB((prevState) => ({ ...prevState, direction: DIRECTIONS.RIGHT }));
       }
-      setCount(count + 1)
     }
-    createMatrix(configs.cant_fil, configs.cant_col)
-    setCount(1)
-
     document.addEventListener("keydown", handleKeyDown);
-
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
+      clearInterval(intervalId)
     }
   }, [])
 
-  useEffect(() => {
-    const intervalId = setTimeout(() => {
-      moveSnakeB()
-    }, velosityB)
-    return () => { clearTimeout(intervalId) }
-  }, [count])
 
 
-  ////
   function reiniciarJuego() {
-    setSnakeB([{ X: 5, Y: 5 }, { X: 5, Y: 6 }, { X: 5, Y: 7 }])
-    setVelosityB(configs.velosityDefault)
+    setInGame(true)
+    setSnakeB({
+      sections: [
+        { X: 5, Y: 5 },
+        { X: 5, Y: 6 },
+        { X: 5, Y: 7 }
+      ],
+      velosity: configs.velosityDefault,
+      direction: DIRECTIONS.UP,
+      points: 0
+    })
     createMatrix(configs.cant_fil, configs.cant_col)
-    setCount(-1)
-    setDirection(DIRECTIONS.UP)
-    setLuser(false)
     moveSnakeB()
   }
 
@@ -132,175 +137,176 @@ export function Tablero({setStart}) {
       OBJECTS.FIVE_MORE_POWER,
       OBJECTS.FIVE_MINUS_POWER,
       OBJECTS.DISFRAGMENT_POWER
-    ]
+    ];
 
-    const positionX = Math.floor(Math.random() * configs.cant_col)
-    const positionY = Math.floor(Math.random() * configs.cant_fil)
-    const whatIsPosition = Math.floor(Math.random() * powers.length)
+    let positionX, positionY, whatIsPosition;
+    let validPosition = false;
 
-    console.log(matriz, matriz[positionY][positionX]);
+    while (!validPosition) {
+      positionX = Math.floor(Math.random() * configs.cant_col);
+      positionY = Math.floor(Math.random() * configs.cant_fil);
+      whatIsPosition = Math.floor(Math.random() * powers.length);
 
-    if (matriz[positionY][positionX] === OBJECTS.WALL) {
-      return printPower(matriz)
+      if (
+        matriz[positionY][positionX] !== OBJECTS.WALL &&
+        matriz[positionY][positionX] !== OBJECTS.HEAD_SNAKEB &&
+        matriz[positionY][positionX] !== OBJECTS.SNAKEB_SECTION &&
+        matriz[positionY][positionX] !== OBJECTS.APPLE
+      ) {
+        validPosition = true;
+      }
     }
-    if (matriz[positionY][positionX] === OBJECTS.HEAD_SNAKEB) {
-      return printPower(matriz)
-    }
-    if (matriz[positionY][positionX] === OBJECTS.SNAKEB_SECTION) {
-      return printPower(matriz)
-    }
-    if (matriz[positionY][positionX] === OBJECTS.APPLE) {
-      return printPower(matriz)
-    }
-    else {
-      setPower({ positionY, positionX, whatIs: powers[whatIsPosition] })
-      return { positionY, positionX, whatIs: powers[whatIsPosition] }
-    }
+
+    setPower({ positionY, positionX, whatIs: powers[whatIsPosition] });
+    return { positionY, positionX, whatIs: powers[whatIsPosition] };
   }
+
 
   function moveSnakeB() {
-    if (loser) return
+    if (!inGame) return;
 
-    const matrizAux = [...matriz];
-    let snakeBAux = [...snakeB]
-    let { X, Y } = snakeBAux[0];
+    setSnakeB((prevState) => {
+      const matrizAux = [...matriz];
+      let snakeBAux = [...prevState.sections];
+      let snakeVelosity = prevState.velosity;
 
+      let { X, Y } = snakeBAux[0];
+      let { X: X2, Y: Y2 } = snakeBAux[1];
 
-    let { X: X2, Y: Y2 } = snakeBAux[1];
-
-    if (direction === DIRECTIONS.DOWN) {
-
-      if (Y + 1 === Y2) {
-        setDirection(DIRECTIONS.UP)
-        return;
-      } else {
-        Y = Y + 1
-      }
-
-    }
-    if (direction === DIRECTIONS.UP) {
-      if (Y - 1 === Y2) {
-        setDirection(DIRECTIONS.DOWN)
-        return;
-      } else {
-        Y = Y - 1
-      }
-
-    }
-    if (direction === DIRECTIONS.LEFT) {
-      if (X - 1 === X2) {
-        setDirection(DIRECTIONS.RIGHT)
-        return;
-      } else {
-        X = X - 1
-      }
-    }
-    if (direction === DIRECTIONS.RIGHT) {
-      if (X + 1 === X2) {
-        setDirection(DIRECTIONS.LEFT)
-        return;
-      } else {
-        X = X + 1
-      }
-    }
-    if (matrizAux[Y][X] === OBJECTS.WALL || matrizAux[Y][X] === OBJECTS.SNAKEB_SECTION) {
-      alert("perdiste")
-      setLuser(true);
-      return
-    }
-    const ultimo = snakeBAux[snakeBAux.length - 1];
-    snakeBAux.unshift({ X, Y })
-    snakeBAux.pop()
-    setSnakeB(snakeBAux)
-
-    for (var i = 0; i < snakeB.length; i++) {
-      if (i == 0) {
-        matrizAux[snakeBAux[i].Y][snakeBAux[i].X] = OBJECTS.HEAD_SNAKEB
-      }
-      else {
-        matrizAux[snakeBAux[i].Y][snakeBAux[i].X] = OBJECTS.SNAKEB_SECTION
-      }
-    }
-
-
-    //comio manzana
-    if (apple.positionX === X && apple.positionY === Y) {
-      matrizAux[ultimo.Y][ultimo.X] = OBJECTS.SNAKEB_SECTION;
-      snakeBAux.push({ Y: ultimo.Y, X: ultimo.X })
-      const { positionY, positionX } = printApple(matriz)
-      matrizAux[positionY][positionX] = OBJECTS.APPLE
-      setPointsB(pointsB + 1)
-    }//comio poder
-    if (power.positionX === X && power.positionY === Y) {
-      let willBeFragment = false;
-
-      if (power.whatIs === OBJECTS.DISFRAGMENT_POWER) {
-        matrizAux[ultimo.Y][ultimo.X] = OBJECTS.SNAKEB_SECTION;
-        willBeFragment = true;
-      }
-      if (power.whatIs === OBJECTS.FAST_POWER) {
-        if (velosityB > configs.minVelosity) {
-          setVelosityB(velosityB - 100)
-        }
-      }
-      if (power.whatIs === OBJECTS.FIVE_MINUS_POWER) {
-        for (let index = 0; index < 5; index++) {
-          if (snakeBAux.length > 2) {
-            const last = snakeBAux.pop()
-            matrizAux[last.Y][last.X] = OBJECTS.GRASS;
+      // Actualizar dirección basada en la dirección actual
+      switch (prevState.direction) {
+        case DIRECTIONS.DOWN:
+          if (Y + 1 === Y2) {
+            return { ...prevState, direction: DIRECTIONS.UP };
+          } else {
+            Y += 1;
           }
+          break;
+        case DIRECTIONS.UP:
+          if (Y - 1 === Y2) {
+            return { ...prevState, direction: DIRECTIONS.DOWN };
+          } else {
+            Y -= 1;
+          }
+          break;
+        case DIRECTIONS.LEFT:
+          if (X - 1 === X2) {
+            return { ...prevState, direction: DIRECTIONS.RIGHT };
+          } else {
+            X -= 1;
+          }
+          break;
+        case DIRECTIONS.RIGHT:
+          if (X + 1 === X2) {
+            return { ...prevState, direction: DIRECTIONS.LEFT };
+          } else {
+            X += 1;
+          }
+          break;
+        default:
+          break;
+      }
 
+      // Verificar colisiones
+      if (matrizAux[Y][X] === OBJECTS.WALL || matrizAux[Y][X] === OBJECTS.SNAKEB_SECTION) {
+        alert("perdiste");
+        setInGame(false);
+        return prevState;
+      }
+
+      // Actualizar la posición de la serpiente
+      const tail = snakeBAux[snakeBAux.length - 1];
+      snakeBAux.unshift({ X, Y });
+      snakeBAux.pop();
+
+      // Actualizar la matriz
+      for (let i = 0; i < snakeBAux.length; i++) {
+        if (i === 0) {
+          matrizAux[snakeBAux[i].Y][snakeBAux[i].X] = OBJECTS.HEAD_SNAKEB;
+        } else {
+          matrizAux[snakeBAux[i].Y][snakeBAux[i].X] = OBJECTS.SNAKEB_SECTION;
         }
-
       }
-      if (power.whatIs === OBJECTS.ONE_MINUS_POWER) {
-        if (snakeBAux.length > 2) {
-          const last = snakeBAux.pop()
-          matrizAux[last.Y][last.X] = OBJECTS.GRASS;
+
+      // Comió manzana
+      if (apple.positionX === X && apple.positionY === Y) {
+        matrizAux[tail.Y][tail.X] = OBJECTS.SNAKEB_SECTION;
+        snakeBAux.push({ Y: tail.Y, X: tail.X });
+        const { positionY, positionX } = printApple(matriz);
+        matrizAux[positionY][positionX] = OBJECTS.APPLE;
+        setPointsB((prevPoints) => prevPoints + 1);
+      }
+
+      // Comió poder
+      if (power.positionX === X && power.positionY === Y) {
+        switch (power.whatIs) {
+          case OBJECTS.FAST_POWER:
+            alert("Fast");
+            if (snakeVelosity > configs.minVelosity) {
+              snakeVelosity=snakeVelosity-100;
+              setVelosityB((prevVelosity) => prevVelosity - 100);
+            }
+            break;
+          case OBJECTS.FIVE_MINUS_POWER:
+            alert("-5");
+            for (let index = 0; index < 5; index++) {
+              if (snakeBAux.length > 2) {
+                const last = snakeBAux.pop();
+                matrizAux[last.Y][last.X] = OBJECTS.GRASS;
+              }
+            }
+            break;
+          case OBJECTS.ONE_MINUS_POWER:
+            alert("-1");
+            if (snakeBAux.length > 2) {
+              const last = snakeBAux.pop();
+              matrizAux[last.Y][last.X] = OBJECTS.GRASS;
+            }
+            break;
+          case OBJECTS.FIVE_MORE_POWER:
+            alert("+5");
+            for (let index = 0; index < 5; index++) {
+              snakeBAux.push(tail);
+            }
+            break;
+          case OBJECTS.SLOW_POWER:
+            alert("Slow");
+            if (velosityB < configs.maxVelosity) {
+              setVelosityB((prevVelosity) => prevVelosity + 100);
+            }
+            break;
+          default:
+            break;
         }
-
-      }
-      if (power.whatIs === OBJECTS.FIVE_MORE_POWER) {
-        for (let index = 0; index < 5; index++) {
-          snakeBAux.push(ultimo)
-
-        }
-
-      }
-      if (power.whatIs === OBJECTS.SLOW_POWER) {
-        if (velosityB < configs.maxVelosity) {
-          setVelosityB(velosityB + 100)
-        }
-
-      }
-      if (!willBeFragment) {
-        matrizAux[ultimo.Y][ultimo.X] = OBJECTS.GRASS;
+        matrizAux[tail.Y][tail.X] = OBJECTS.GRASS;
+        const { positionY, positionX } = printPower(matriz);
+        matrizAux[positionY][positionX] = power.whatIs;
+      } else {
+        matrizAux[tail.Y][tail.X] = OBJECTS.GRASS;
       }
 
-      const { positionY, positionX } = printPower(matriz)
-      matrizAux[positionY][positionX] = power.whatIs
-    }
-    else {
-      if (count > 10) {
-        const { positionY, positionX } = printPower(matriz)
-        matrizAux[positionY][positionX] = power.whatIs
-      }
-      matrizAux[ultimo.Y][ultimo.X] = OBJECTS.GRASS;
-    }
-    setSnakeB(snakeBAux)
-    setCount(count + 1)
-    setMatriz(matrizAux)
+      setMatriz(matrizAux);
 
+      return {
+        ...prevState,
+        sections: snakeBAux
+      };
+    });
   }
+
 
 
   return (
     <>
-      <p>{count}</p>
-      <div> Puntuación: {pointsB}</div>
-      <div> Velocidad de la serpiente: {100 - (velosityB * 100 / configs.maxVelosity)}</div>
-      <div> Tamaño de la serpiente: {snakeB.length}</div>
-      <div className='tablero' style={{display: "grid",gridTemplateColumns: "repeat("+configs.cant_col+", 2fr)"}}>
+      <div>
+        
+        <div> Puntuación: {snakeB.points}</div>
+        <div> Velocidad de la serpiente: {100 - (snakeB.velosity * 100 / configs.maxVelosity)}</div>
+        <div> Tamaño de la serpiente: {snakeB.length}</div>
+        <div>Te comiste un { }</div>
+      </div>
+
+      <div className='tablero' style={{ display: "grid", gridTemplateColumns: "repeat(" + configs.cant_col + ", 30px)", gap: 0 }}>
         {matriz.map((fila) => {
           return fila.map((value) => (
             <Label value={value} />
@@ -309,7 +315,7 @@ export function Tablero({setStart}) {
 
       </div>
       <div>
-        <button className={"btn"} onClick={()=>setStart(false)} >Back </button>
+        <button className={"btn"} onClick={() => setStart(false)} >Back </button>
         <button className={"btn"} onClick={reiniciarJuego} >Reiniciar</button>
       </div>
 
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
 
-export function Welcome({setStart}) {
+export function Welcome({ setStart }) {
 
 
   return (
     <div >
       <h1>Bienvenido a Snake.js</h1>
       <div className="center-in-x">
-        <button 
-        className="btn btn-init"
-        onClick={setStart}
+        <button
+          className="btn btn-init"
+          onClick={setStart}
         >
           Single player
         </button>
-        <button className="btn btn-init">
+        <button className="btn btn-init" onClick={() => { alert("proximamente") }}>
           Multi player
         </button>
 
-        <button className="btn btn-init">
+        <button className="btn btn-init" onClick={() => { alert("proximamente") }}>
           Settings
         </button>
-        <button className="btn btn-init">
+        <button className="btn btn-init" onClick={() => { alert("proximamente") }}>
           Help
         </button>
       </div>
